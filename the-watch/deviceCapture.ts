e327a62f838f4fe66df47630fb7fe9c19eb5261f
Lionel <lionel@morrison.mobi>
2023-08-21 20:05:19 -0400

    Fixed write issue and created time

--- the-watch/deviceCapture.ts
+++ the-watch/deviceCapture.ts
@@ -21,7 +21,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
 
     try {
         const data: LogDeviceStatus = {
-            created: Date.now().toString(),
+            created: status.context.timeOfSample.toString(),
             accountId: params?.accountId as string,
             deviceId: status.context.deviceMac,
             deviceType: status.context.deviceType,
@@ -35,24 +35,22 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
             Item: data,
         });
 
-        console.log(data);
-
         const latestData = new PutCommand({
             TableName: 'latest',
             Item: data,
         });
 
-        // const response = await docClient.send(logData);
-        // await docClient.send(latestData);
+        const response = await docClient.send(logData);
+        await docClient.send(latestData);
 
-        // if (!response) {
-        //     return {
-        //         statusCode: 500,
-        //         body: JSON.stringify({
-        //             message: `Failed to log status for ${params}`,
-        //         }),
-        //     };
-        // }
+        if (!response) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    message: `Failed to log status for ${params}`,
+                }),
+            };
+        }
 
         return {
             statusCode: 200,
